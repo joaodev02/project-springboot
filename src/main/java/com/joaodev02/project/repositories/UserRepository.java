cafaa7786361fc6a88c93f0f487469dc02d89af7
joaodev02 <joao.dev02@outlook.com>
2023-05-14 18:56:57 -0300

    Service layer, component registration

--- src/main/java/com/joaodev02/project/repositories/UserRepository.java
+++ src/main/java/com/joaodev02/project/repositories/UserRepository.java
@@ -1,2 +1,9 @@
-package com.joaodev02.project.repositories;public class UserRepository {
+package com.joaodev02.project.repositories;
+
+import com.joaodev02.project.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
 }
